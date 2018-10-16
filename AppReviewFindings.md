9f0dee265af27829c5f50a746fb65fc3142945c2
debrashere <debrashere@gmail.com>
2018-10-15 18:49:05 -0700

    updates for functionality

--- AppReviewFindings.md
+++ AppReviewFindings.md
@@ -6,23 +6,75 @@ Link to the live demo: https://debrashere.github.io/MenuAndEventPlanner/
 
 ### Summary
 The first draft of this app uses mock data for the initial menu and event details. 
-
+I've reviewed this application with at least 4 people who would use this type of app. Following are my findings.
   #### What you can do:
     Search for recipes.
     Make changes to the event details such as address and event name. Note: this data is not stored when the page is refreshed, the mock data will appear. 
 
-### User Review Notes
-I've reviewed this application with at least 4 people who would use this type of app. Following are my findings.
+  #### Overview of findings  - what users would like to do 
+    When screen is large enough, display the google map beside the event location
+    Make the recipe results smaller and display more then 3 results on the screen
+    Functionality to manage menu items, such as deletion, renaming, assigning atttendee as contributer to a menu item
+    Functionality to have multiple  hosts.
+    On smaller screen, reduce the size of the recipe display.
+    Display headcount of the total expected attendees.
+    Allow management of the attendees such as adding, removing attendees.
+    Display next to the menu items which person(s) contributed to the item.
+    When user clicks a contributer name, display all items which has been assigned to this person.
+    On larger screens display the courses in columns so that you can possibly see all courses without scrolling in the menu section.
+    
 
-- Observations
-    - When screen is large enough, display the google map beside the event location
-    - Make the recipe results smaller and display more then 3 results on the screen
-    - Functionality to manage menu items, such as deletion, renaming, assigning atttendee as contributer to a menu item
-    - Functionality to have multiple  hosts.
-    - On smaller screen, reduce the size of the recipe display.
-    - Display headcount of the total expected attendees.
-    - Allow management of the attendees such as adding, removing attendees.
-    - Display next to the menu items which person(s) contributed to the item.
-    - When user clicks a contributer name, display all items which has been assigned to this person.
-    - On larger screens display the courses in columns so that you can possibly see all courses without scrolling in the menu section.
-  
+### Examples of requested fixes
+
+To find recipes:
+
+```
+  Enter ingredient, dish name etc in "Input" box for recipe search and hit enter.
+  Collection of recipes will display below the search box. 
+  Review the results by paging thru them. 
+```
+
+To select a recipe to add to the menu
+
+```
+Within each recipe box is text "Add recipe to menu".
+Below that are links for each couse.
+Click on the link for the course you would like to add the recipe to.
+
+Scroll down to the menu section and the recipe name from the search results should appear.
+```
+
+To update the location of the event.
+
+```
+Next to the event name, click the "edit" link.
+An input form should appear at the top of the screen.
+Change the address of the event and click the "Submit" button.
+
+The address and the google map should be updated with the new address.
+```
+
+## Screen Shots
+
+![Recipe Search Results](https://raw.githubusercontent.com/debrashere/MenuAndEventPlannerLayout/master/Layout1.png)
+
+![Event location and google map](https://raw.githubusercontent.com/debrashere/MenuAndEventPlannerLayout/master/layoutEventDetails.png)
+
+![Event menu](https://raw.githubusercontent.com/debrashere/MenuAndEventPlannerLayout/master/layoutMenuDetails.png)
+
+
+## Built With
+
+* [VsCode](http://www.dropwizard.io/1.0.2/docs/) - The web framework used
+* [Food2Network API](https://Food2Fork.com/) - API used for recipe search
+* [Google Maps API](https://maps.googleapis.com/) - Used to generate googole maps
+* [Photos from unsplash.com](https://unsplash.com/) - Used for heading background* 
+* [Target icon from iconfinder.com](https://www.iconfinder.com/) - Used in Google Map to show event location 
+
+## Versioning
+
+ [Github](https://github.com/) is used for versioning.
+
+## Authors
+
+* **Debra Odom** - *Initial work* 
--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const GOOGLE_MAP_URL = "https://maps.googleapis.com/maps/api/staticmap";
 const GOOGLE_MAP_KEY = "AIzaSyDKhoEyXMCpnWzwGiEqzfnffZEQijVkGek";
 const FOOD2FORK_KEY = 'a5b4b941891f56d391acab1758d6f6fd';
 const FOOD2FORK_URL  = "https://www.food2fork.com/api/search";
-let FOOD2FORK_PAGE = 1;
-
+let   FOOD2FORK_PAGE = 1;
+const RECIPES_TO_DISPLAY= 5;
 
 let food2Fork_data = {
     food2ForkResults: null,
@@ -102,7 +102,7 @@ let MENU =
                      {menuItem: "Berried Beer Punch",
                   contributors: [ { contributor: "Mason"}]},
                      {menuItem: "Sodas",
-                  contributors: [ { contributor: "Jason"}]}, 
+                  contributors: [ { contributor: "Julia"}]}, 
                      {menuItem: "Beers",
                   contributors: [ { contributor: "Hubert"}]},  
                      {menuItem: "Magaritas to go",
@@ -151,34 +151,56 @@ function submitAPIForRecipes(queryString) {
     $('#js-error-message').prop("hidden", false);
     $('#js-error-message').text(`Something went wrong: ${error}`);
   });
+  
 } 
 
 function generateGoogleMap() {
    const address = `${encodeURIComponent(EVENT.address1)},${encodeURIComponent(EVENT.city)},${encodeURIComponent(EVENT.state)}+${EVENT.zip}`;
    const markers = `markers=color:red%7Clabel:L%7C${address}`;
-   const googleMapUrl = `${GOOGLE_MAP_URL}?center=${address}&zoom=15&size=400x400&${markers}&key=${GOOGLE_MAP_KEY}`; 
+   const googleMapUrl = `${GOOGLE_MAP_URL}?center=${address}&zoom=15&size=300x250&${markers}&key=${GOOGLE_MAP_KEY}`; 
    let mappedEvent = `<img src="${googleMapUrl}" alt="google map image" />`;
    $(".js-map-canvas").html(mappedEvent);
 }
 
 function generateEventDetails() {
   let eventDetails = `
-    <p><h2>Event: ${EVENT.name}</h2> <a role="link" class="edit js-edit" alt="edit event details" id="EditHost" href=#>edit</a></p>
-    <p class="eventLocation"><h3>Host: ${EVENT.host}</h3></p>
-    <p class="eventLocation"><h3>Date: </h3> <span role="datetime" id="event-date" class="js-event-date"> ${EVENT.date}</span></p>        
-    <p class="eventLocation"><h3>Location: </h3> <span id="event-location" class="js-event-location"> ${EVENT.location}</span></p>  
-    <p class="eventLocation"><h3>Address: </h3>  <address id="event-address" class="js-event-address">${EVENT.address1}, ${EVENT.city}, ${EVENT.state} ${EVENT.zip}</address></p>
-    <p class="eventLocation"><h3>Start time:</h3><span id="event-startTime"> ${EVENT.startTime}</span></p>`;
+    <h2>Event: ${EVENT.name}</h2> <a role="link" class="js-edit" alt="edit event details" id="EditHost" href=#>edit</a>
+    <div  class="eventLocation"><h3>Host: ${EVENT.host}</h3></div>
+    <div  class="eventLocation"><h3>Date: </h3> <span role="datetime" id="event-date" class="js-event-date"> ${EVENT.date}</span></div>
+    <div  class="eventLocation"><h3>Location: </h3> <span id="event-location" class="js-event-location"> ${EVENT.location}</span></div>
+    <div  class="eventLocation"><h3>Address: </h3>  <address id="event-address" class="js-event-address">${EVENT.address1}, ${EVENT.city}, ${EVENT.state} ${EVENT.zip}</address></div>
+    <div  class="eventLocation"><h3>Start time:</h3><span id="event-startTime"> ${EVENT.startTime}</span></div>`;
     $(".js-event-hosts").html(eventDetails);
     generateGoogleMap();
     watchEditLinkClick();
 }
+ 
+  function showContributorDetails(thisContributor) {   
+    let courseAndItems = `<h3>${thisContributor}</h3>  <a id="showMenu" href="#" class=" js-edit js-show-menu"> Show Menu</a> <br> `;  
+    let menuItms = "";
+    let courseHeading = "";
+    MENU.courses.map( function (thisCourse) {       
+      jQuery.each( thisCourse.menuItems, function( i, menuItems ) {
+        if ( menuItems.contributors.filter( c => c.contributor == `${thisContributor}`).length > 0) {  
+           courseHeading = `<h3>${thisCourse.course}</h3>`; 
+           menuItms += `<p class="contributed">${menuItems.menuItem}</p>`           
+        } 
+        courseAndItems += `${courseHeading}${menuItms}`;
+        menuItms = "";
+        courseHeading= "";
+    });   
+  }); 
+  
+  $(".js-menu").html(courseAndItems);  
+  $(".js-menu").prop("hidden", false);  
+  watchEditLinkClick(); 
+  }
 
-function generaterCoursesLinks(recipeId) {  
+function generateCoursesLinks(recipeId) {  
   let coursesOption = "";
   MENU.courses.map( function(courses) {
    coursesOption+=`
-      <a class="add-recipe-to js-add-recipe-to" alt="add ${courses.course} to menu" id="add-${courses.course}-${recipeId}" href=#>${courses.course}</a>, `;   
+      <a class="add-recipe-to js-add-recipe-to" id="add-${courses.course}-${recipeId}" href=#>${courses.course}</a>, `;   
   });
   return coursesOption.substring(0, coursesOption.length-2);
 }
@@ -186,26 +208,26 @@ function generaterCoursesLinks(recipeId) {
 function continueRecipesAfterPaging(searchResults, navigation) { 
   const data = searchResults.food2ForkResults;  
   if (navigation == "next" && searchResults.resultsThru < data.count) {      
-      searchResults.resultsFrom += 3;
-      searchResults.resultsThru += 3;
+      searchResults.resultsFrom += RECIPES_TO_DISPLAY;
+      searchResults.resultsThru += RECIPES_TO_DISPLAY;
       $( ".search-nav" ).prop( "hidden", false )  
   } 
   if (navigation == "prev" && searchResults.resultsFrom > 0) {   
-    searchResults.resultsFrom -= 3;
-    searchResults.resultsThru -= 3;
+    searchResults.resultsFrom -= RECIPES_TO_DISPLAY;
+    searchResults.resultsThru -= RECIPES_TO_DISPLAY;
     $( ".search-nav" ).prop( "hidden", false )      
   }  
   
   if (navigation == "next" && searchResults.resultsThru > searchResults.maxResults) {
     searchResults.resultsFrom = 0;
-    searchResults.resultsThru = 3;
+    searchResults.resultsThru = RECIPES_TO_DISPLAY;
     searchResults.page += 1;      
     submitAPIForRecipes(searchResults.query); 
     return false;
   }
 
   if (navigation == "prev" && searchResults.resultsFrom == 0 && searchResults.page >0 ) {
-    searchResults.resultsFrom = searchResults.maxResults - 3;;
+    searchResults.resultsFrom = searchResults.maxResults - RECIPES_TO_DISPLAY;
     searchResults.resultsThru = searchResults.maxResults;
     searchResults.page -= 1;      
     submitAPIForRecipes(searchResults.query); 
@@ -213,33 +235,12 @@ function continueRecipesAfterPaging(searchResults, navigation) {
   }
 
   if (navigation == "prev" && searchResults.resultsFrom > 0) {   
-    searchResults.resultsFrom -= 3;
-    searchResults.resultsThru -= 3;
+    searchResults.resultsFrom -= RECIPES_TO_DISPLAY;
+    searchResults.resultsThru -= RECIPES_TO_DISPLAY;
     $( ".search-nav" ).prop( "hidden", false )      
   }    
   return true;
 }
-
-function watchSubmitCourseButtonClick() {       
-  $('#submitEvent').click(event => {
-  event.preventDefault();
-  if (!eventEditsAreValid())  return;    
-    EVENT.name =  $('#eventName').val();   
-    EVENT.data = $("#eventDate").val();
-    EVENT.location = $("#eventLocation").val();
-    EVENT.hostName = $("#hostName").val();
-    EVENT.address1 = $("#address1").val();
-    EVENT.city = $("#city").val();
-    EVENT.state = $("#state").val();
-    EVENT.zip = $("#zip").val();
-    EVENT.startTime = $("#startTime").val();
-    $(".js-search").prop("hidden", false);  
-    $('.js-search-button').prop("hidden", false);
-    generateEventDetails(); 
-    $(".js-edit-event").prop("hidden", true);
-  });  
-}
-
 function renderRecipe(searchResults, navigation) { 
   let slideCount = 1; 
   $( ".slider-gallery" ).html('<div class="row">');
@@ -248,7 +249,7 @@ function renderRecipe(searchResults, navigation) {
   const data = searchResults.food2ForkResults;     
   data.recipes.slice(searchResults.resultsFrom,searchResults.resultsThru).map( function(recipe) {    
   const recipeId= `recipe${slideCount}`;
-  const coursesLinks = generaterCoursesLinks(recipeId);
+  const coursesLinks = generateCoursesLinks(recipeId);
 
   let thumb =  
     `<div class="recipe">
@@ -275,10 +276,10 @@ function storeFood2ForkResults(data) {
   food2ForkResults: data,
              page:0,
       resultsFrom: 0,
-      resultsThru: 3,
+      resultsThru: RECIPES_TO_DISPLAY,
        maxResults:30    
     }
-    data.count > 3
+    data.count > RECIPES_TO_DISPLAY
       ? $( ".search-nav" ).prop( "hidden", false ) 
       : $( ".search-nav" ).prop( "hidden", true ) ;
 }
@@ -313,7 +314,7 @@ function handleNextPageClicked() {
  
   function showFormEditHost() {
     let formInputs = `
-      <fieldset class="edit-event">
+      <fieldset class="edit-form">
         <legend><h2>Host details and event name</h2></legend>
        <p> <label for="eventName" class="edit-label"><strong>Event Name: </strong></label> <input type="text" id="eventName" value="${EVENT.name}" required /></p>
        <p> <label for="eventDate" class="edit-label"><strong>Event Date: </strong></label>  <input role="datetime" type="datetime"  id="eventDate" value="${EVENT.date}" /></p>
@@ -326,23 +327,32 @@ function handleNextPageClicked() {
        <p> <label for="startTime" class="edit-label"><strong>Start Time: </strong></label>  <input type="text"  id="startTime" value="${EVENT.startTime}" /></p>
       </fieldset>`;
 
-      $(".js-edit-event-details").html(formInputs);   
-      $(".js-edit-event").prop("hidden", false);
-      $(".js-search").prop("hidden", true);
+      $(".js-edit-event").html(formInputs);   
+      $(".js-edit-event").prop("hidden", false);     
+      $(".js-edit-button").prop("hidden", false);
+      $(".js-edit-menu").prop("hidden", false);
+      $(".js-search").prop("hidden", true);      
       $('.js-search-button').prop("hidden", true);
       $("#eventName").prop('required',true);
   }
   
 function generateMenu() {   
   let menuDivs = "";
+  let contributorSpan = "";
   MENU.courses.map( function(courses) {
-    menuDivs += `<h3 class="menu-course">${courses.course}</h3>`;     
-    courses.menuItems.map( function (menuItems) {   
-          menuDivs+= `<p class="menuItem js-menuItem">${menuItems.menuItem} </p>`;                          
+    menuDivs += `<div class="col course"><h3 class="menu-course" id="bm-${courses.course}">${courses.course}</h3>`;    
+    courses.menuItems.map( function (menuItems) {  
+      menuItems.contributors.map( function(contributors) {     
+        contributorSpan += `  <a href="#" id="conDetails-${contributors.contributor}" class="contributor js-edit js-contributor" alt="Next">${contributors.contributor}</a>`;
+      });     
+      menuDivs+= `<p class="menuItem js-menuItem">${menuItems.menuItem} ${contributorSpan} </p>`;
+      contributorSpan = "";                          
     });
+    menuDivs += "</div>";
   });  
   $(".js-menu").html("<h2>Menu</h2>");  
-  $(".js-menu").append(`<Div class="menuItems js-menuItems">${menuDivs}</Div>` );
+  $(".js-menu").append(`<div class="menuItems js-menuItems">${menuDivs}</div>` ); 
+  watchEditLinkClick();
 }
 
 function addRecipeToMenu(id) {
@@ -358,11 +368,21 @@ function addRecipeToMenu(id) {
 
 function watchEditLinkClick() {       
   $('.js-edit').click(event => {
-    event.preventDefault();
-    const editItem = event.currentTarget.id;          
-    window[`showForm${editItem}`]();
+    event.preventDefault();  
+    const editItem = event.currentTarget.id; 
+    console.log(editItem.substring(0, 10)) ;
+    if (editItem == "EditHost") {
+       showFormEditHost();
+    }
+    else if  (editItem =="showMenu") {    
+      generateMenu();   
+    }
+    else if  (editItem.substring(0, 10) == "conDetails") {    
+      showContributorDetails(editItem.split('-')[1]);    
+    }
   });  
 }
+
 function watchAddLinkClick() {       
   $(".js-add-recipe").click(function(event) { 
     event.preventDefault();
@@ -396,8 +416,64 @@ function eventEditsAreValid() {
     return  $('#js-error-message').text().length == 0;
 }
 
+function updateEventDetails() {       
+    if (eventEditsAreValid()) {
+      EVENT.name =  $('#eventName').val();   
+      EVENT.data = $("#eventDate").val();
+      EVENT.location = $("#eventLocation").val();
+      EVENT.hostName = $("#hostName").val();
+      EVENT.address1 = $("#address1").val();
+      EVENT.city = $("#city").val();
+      EVENT.state = $("#state").val();
+      EVENT.zip = $("#zip").val();
+      EVENT.startTime = $("#startTime").val();
+      $(".js-search").prop("hidden", false);  
+      $('.js-search-button').prop("hidden", false);
+      generateEventDetails(); 
+      $(".js-edit").prop("hidden", true);
+      $(".js-edit-form").prop("hidden", true);
+      location.hash = "bm-event";
+    }
+}
+
 function watchSubmitEventButtonClick() {       
   $('#submitEvent').click(event => {
+    event.preventDefault();  
+    updateEventDetails()
+  });  
+}
+
+function watchSubmitEventButtonClickOLD() {       
+  $('#submitEvent').click(event => {
+    event.preventDefault();
+    updateEventDetails();
+    if (eventEditsAreValid()) {
+      EVENT.name =  $('#eventName').val();   
+      EVENT.data = $("#eventDate").val();
+      EVENT.location = $("#eventLocation").val();
+      EVENT.hostName = $("#hostName").val();
+      EVENT.address1 = $("#address1").val();
+      EVENT.city = $("#city").val();
+      EVENT.state = $("#state").val();
+      EVENT.zip = $("#zip").val();
+      EVENT.startTime = $("#startTime").val();
+      $(".js-search").prop("hidden", false);  
+      $('.js-search-button').prop("hidden", false);
+      generateEventDetails(); 
+      $(".js-edit-form").prop("hidden", true);
+      location.hash = "bm-event";
+    }
+  });  
+}
+
+function watchResetEventButtonClick() {       
+  $('#resetEvent').click(event => {
+     showFormEditHost();    
+  });  
+}
+
+function watchSubmitMenuButtonClick() {       
+  $('#submitMenu').click(event => {
     event.preventDefault();
     if (eventEditsAreValid()) {
       EVENT.name =  $('#eventName').val();   
@@ -412,13 +488,14 @@ function watchSubmitEventButtonClick() {
       $(".js-search").prop("hidden", false);  
       $('.js-search-button').prop("hidden", false);
       generateEventDetails(); 
-      $(".js-edit-event").prop("hidden", true);
+      $(".js-edit-form").prop("hidden", true);
+      location.hash = "bm-event";
     }
   });  
 }
-
-function watchResetEventButtonClick() {       
-  $('#resetEvent').click(event => {
+     
+function watchResetMenuButtonClick() {       
+  $('#resetMenu').click(event => {
   if (!eventEditsAreValid())  return;
      showFormEditHost();
   });  
@@ -426,9 +503,11 @@ function watchResetEventButtonClick() {
 
 function watchCancelEventButtonClick() {       
   $('#cancelEvent').click(event => {  
-    $(".js-edit-event").prop("hidden", true);
+    $(".js-edit-form").prop("hidden", true);
+    $(".js-edit-button").prop("hidden", true);
     $(".js-search").prop("hidden", false);  
     $('.js-search-button').prop("hidden", false);
+    location.hash = "bm-event";
   });  
 }
 
@@ -438,9 +517,10 @@ function setupHandleEvents() {
       watchSubmitEventButtonClick();
       watchResetEventButtonClick();
       watchCancelEventButtonClick();
+      watchSubmitMenuButtonClick();
+      watchResetMenuButtonClick();
       watchSearchButtonClick(); 
       watchEditLinkClick(); 
-
       watchAddLinkClick();
       watchAddLToinkClick();
       handlePrevPageClicked();
